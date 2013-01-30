ab787281db8559242cc45b5c0687739c03a6c4d4
Guilherme Miyahira <guilherme.miyahira@gmail.com>
2013-01-30 10:35:39 -0200

    testando git

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -5,10 +5,28 @@ import play.mvc.*;
 
 import views.html.*;
 
+import models.Task;
+import play.data.Form;
+
+import play.db.ebean.Model;
+import java.util.List;
+import static play.libs.Json.toJson;
+
 public class Application extends Controller {
   
-  public static Result index() {
-    return ok(index.render("Your new application is ready."));
-  }
+	public static Result index() {
+		return ok(index.render("hello, world", form(Task.class)));
+	}
   
+	public static Result addTask() {
+		Form<Task> form = form(Task.class).bindFromRequest();
+		Task task = form.get();
+		task.save();
+		return redirect(routes.Application.index());
+	}
+  
+	public static Result getTasks() {
+		List<Task> tasks = new Model.Finder(String.class, Task.class).all();
+		return ok(toJson(tasks));
+	}
 }
--- project/Build.scala
+++ project/Build.scala
@@ -7,12 +7,14 @@ object ApplicationBuild extends Build {
     val appName         = "play2test"
     val appVersion      = "1.0-SNAPSHOT"
 
+	
     val appDependencies = Seq(
-      // Add your project dependencies here,
+	  "org.webjars" % "webjars-play" % "2.0",
+      "org.webjars" % "bootstrap" % "2.2.2-1"
     )
 
     val main = PlayProject(appName, appVersion, appDependencies, mainLang = JAVA).settings(
-      // Add your own project settings here      
+      
     )
 
 }
